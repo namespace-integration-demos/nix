5b7150b22008089210c8c1e46a402216a02fa46c
Niklas Gehlen <niklas@namespacelabs.com>
2024-12-02 16:25:16 +0100

    test with more build inputs (#2)
    * test with more build inputs
    
    * Stress test the cache more.
    
    * fix invocation
    
    * no ls
    
    * larger test
    
    * more to download

--- flake.nix
+++ flake.nix
@@ -13,5 +13,37 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gopls
+          go-outline
+          go-tools
+          upx
+          yarn
+          pre-commit
+
+          goreleaser
+          gh
+
+          eksctl
+          postgresql
+
+          git
+          nodejs
+          crane
+          kubectl
+          awscli2
+          jq
+          aws-iam-authenticator
+          google-cloud-sdk
+          
+          clang-tools
+          brotli
+          sqlite
+          unzip
+          cargo
+          jdk8
+        ];
+      };
     });
 }
